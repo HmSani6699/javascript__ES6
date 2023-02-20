b8d12cce733fa8974e1f7cec6b7d30c8d8d936e2
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-20 14:02:56 +0600

    ad the template string part

---
+++ template.js
@@ -0,0 +1,12 @@
+const allFriends = `
+Md. Noman Mia 
+Md. josim 
+Md. korim
+Md. Raju
+`;
+
+const num1 = 20;
+const num2 = 30;
+const total = `First Amount : ${num1} and Second Amount : ${num2} total Amount ${num1 + num2} !!`
+
+console.log(total)
