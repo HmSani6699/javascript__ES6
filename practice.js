a5878f2215c740b66eb5a7bf33b240f90db59e48
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-20 20:01:45 +0600

    complete practice for and five

--- practice.js
+++ practice.js
@@ -13,3 +13,28 @@ I love eat biryani.
 // practice 3
 const sum = (num1, num2 = 6) => num1 + num2;
 // console.log(sum(5))
+
+// practice 4
+const friends = ['Noman', 'Rakib', 'Thalha', 'Mamun', 'Josim', 'sani']
+const largeName = (friends) => {
+    const newArray = [];
+    for (const friend of friends) {
+        if (friend.length % 2 == 0) {
+            newArray.push(friend)
+        }
+    }
+    return newArray
+}
+// console.log(largeName(friends))
+
+
+// practice 5
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8, 9, 10];
+
+const maxNumber = (arr1, arr2) => {
+    const newArr = [...arr1, ...arr2];
+    return Math.max(...newArr)
+}
+// console.log(maxNumber(arr1, arr2))
+
