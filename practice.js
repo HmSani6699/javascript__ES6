15d51e5599355ffdc3d7ab805a6cfc40659c7ea3
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-20 19:32:54 +0600

    problem two solve complete

--- practice.js
+++ practice.js
@@ -1,3 +1,11 @@
 // practice 1
 const multiply = (num1, num2, num3) => num1 * num2 * num3;
 // console.log(multiply(5, 5, 5))
+
+//practice 2
+const myLove = `
+I love web developer.
+I love code.
+I love eat biryani.
+`;
+console.log(myLove)
