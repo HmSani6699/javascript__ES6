f98d136e1acd06f6334643c5910880669acc8b31
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-21 18:55:04 +0600

    solve the 3 problem

--- ES6-class-32/practice.js
+++ ES6-class-32/practice.js
@@ -11,4 +11,24 @@ for (let i = 0; i < number.length; i++) {
     allEvent.push(event)
 }
 
-// console.log(allEvent)
+// console.log(allEvent);
+
+// practice 2
+//now return all element witch are divisible 10 use the filter method
+const array = [33, 50, 79, 78, 90, 101, 30, 10];
+const result = array.filter(num => num % 10 === 0);
+// console.log(result)
+
+// practice 3
+//sem task using the find method
+const result1 = array.find(num => num % 10 === 0);
+// console.log(result1)
+
+
+// practice 4
+//sum all element using the reduce method
+const array2 = [1, 9, 17, 22];
+const sum = array2.reduce((accumulated, current) => accumulated + current, 1)
+// console.log(sum)
+
+
